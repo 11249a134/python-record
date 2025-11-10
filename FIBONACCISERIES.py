5b48a62e95847fe4ee04c89d7c71cf2c288be4a5
11249a134 <11249a134@kanchiuniv.ac.in>
2025-11-10 15:51:23 +0530

    Update FIBONACCISERIES.py

--- FIBONACCISERIES.py
+++ FIBONACCISERIES.py
@@ -1,3 +1,18 @@
+//Aim:To write a Python program that generates the Fibonacci sequence up to N terms, where N > 0. The program should display an error message if the entered value of N is not greater than zero.
+//Algorithm:
+1.Start the program.
+2.Accept a value N from the user.
+3.Check if N > 0:
+4.If not, display an error message and stop the program.
+5.Define a function fibonacci(N) to generate the Fibonacci sequence.
+6.Inside the function:
+7.Initialize the first two numbers as 0 and 1.
+8.Use a loop to generate the next terms using the formula:
+9.Fn = Fn-1 + Fn-2.
+10.Print the sequence up to N terms.
+11.Call the function with the user-provided value.
+12.Stop the program.
+//Program:
 def fibonacci(N):
     a, b = 0, 1
     print("Fibonacci sequence:")
@@ -9,3 +24,4 @@ if N <= 0:
     print("Error: Please enter a value greater than 0.")
 else:
     fibonacci(N)
+//Result: A program that generates the Fibonacci sequence up to N terms, where N > 0 is executed succesfully
